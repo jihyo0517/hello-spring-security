dc9bb8098220c0b8b5d4b3bba7f046cec8992246
jihyo0517 <105144585+jihyo0517@users.noreply.github.com>
2026-06-02 19:57:33 +0900

    feat: 페이지네시션, 검색 기능 추가
    페이징 기능은 구현됨. 페이지 이동 UI미구현으로 UI상을로만 안뜸

--- src/main/java/kr/ac/hansung/service/ProductService.java
+++ src/main/java/kr/ac/hansung/service/ProductService.java
@@ -4,6 +4,8 @@ import kr.ac.hansung.dto.ProductDto;
 import kr.ac.hansung.entity.Product;
 import kr.ac.hansung.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +14,17 @@ import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
+    // 전체 목록 페이징
+    public Page<Product> getProducts(Pageable pageable){
+        return productRepository.findAll(pageable);
+    }
+
+    // 키워드 검색 + 페이징
+    public Page<Product> searchProducts(String keyword, Pageable pageable){
+        return productRepository.findByNameContaining(keyword, pageable);
+    }
 
     private final ProductRepository productRepository;
 
